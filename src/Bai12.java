d08dfcdad9a670392d3124cf4b33f5c58262cf46
hungtuan <hungtuan2072000@gmail.com>
2023-03-20 19:20:09 +0700

    Xeploaidiem

---
+++ src/Bai12.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Bai12 {
+    public static void main(String[] args) {
+        //nhap diem vao ban phim
+        float dtb;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao diem trung binh:");
+        dtb = sc.nextFloat();
+        String xeploai = (dtb >= 8) ? "xep loai gioi" :
+                ((dtb >= 6.5 && dtb < 8) ? "xep loai kha" :
+                        ((dtb >= 5 && dtb < 6.5) ? "xep loai trung binh" : "xep loai yeu"));
+        System.out.println("Ban" + xeploai);
+
+
+    }
+}
